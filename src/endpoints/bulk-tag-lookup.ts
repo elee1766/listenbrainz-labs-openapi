2b966e0c2a07a807f5020f21d2ae5c4ad387dc1d
a <a@tuxpa.in>
2024-12-16 22:06:05 -0600

    noot

--- src/endpoints/bulk-tag-lookup.ts
+++ src/endpoints/bulk-tag-lookup.ts
@@ -5,7 +5,7 @@ import { createGetPostPair } from "../common/createPair";
 
 export const BulkTagLookup = createGetPostPair({
   path: "/bulk-tag-lookup/json",
-  description: "bulk-tag-lookup",
+  description: "Bulk MusicBrainz Tag/Popularity by recording MBID Lookup",
   params:  z.object({
     recording_mbid: z.string().uuid().openapi({
       example: "7db2c09d-345f-46e6-900e-0f4499e29c3d",
@@ -25,6 +25,7 @@ export const BulkTagLookup = createGetPostPair({
             }),
             percent: z.number().openapi({
               example: 0.022332730560578662,
+              description: "the popularity of the track in the context of the tag",
             }),
             source: z.string().openapi({
               example: "recording",
